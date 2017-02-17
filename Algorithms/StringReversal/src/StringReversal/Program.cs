93e3e35ea7ffd8860bac6a20dd1702efe3888f20
Aashish Bhateja <aashish.bhateja@gmail.com>
2017-02-16 22:14:19 -0800

    Adding solution to git

---
+++ Algorithms/StringReversal/src/StringReversal/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StringReversal
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string str = "reverse this string done finally";
+            string strReverse = string.Empty;
+
+            string[] strArray = str.Split();
+
+            for (int i = strArray.Length-1; i >= 0; i--)
+            {
+                if (strReverse == "")
+                {
+                    strReverse = strArray[i];
+                }
+                else
+                {
+                    strReverse = strReverse + " " + strArray[i];
+                }
+            }
+
+            Console.WriteLine(strReverse);
+            Console.ReadKey();
+        }
+    }
+}

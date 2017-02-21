98ef9e595ba88f22154073f22460cbea43ea792c
Aashish Bhateja <aashish.bhateja@gmail.com>
2017-02-20 18:17:13 -0800

    Added word ladder and valid parenthesis

---
+++ Algorithms/LongestValidParenthesis/LongestValidParenthesis/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LongestValidParenthesis
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(longestValidParentheses("(()()"));
+            Console.ReadKey();
+        }
+
+        public static int longestValidParentheses(string s)
+        {
+            if (s.Length == 0 || s.Length == 1) return 0;
+            Stack<char> stack = new Stack<char>();
+            int result = 0;
+            int length = 0;
+
+            for (int i = 0; i <= s.Length - 1; i++)
+            {
+                char c = s.ElementAt(i);
+                if (stack.Count == 0 && c == ')')
+                {
+                    length = 0;
+                    continue;
+                }
+                if (c == '(')
+                {
+                    stack.Push(c);
+                }
+                else
+                {
+                    stack.Pop();
+                    length += 2;
+                    result = Math.Max(length, result);
+                }
+            }
+
+            return result;
+        }
+        //public static int longestValidParentheses(string s)
+        //{
+        //    Stack<int[]> stack = new Stack<int[]>();
+        //    int result = 0;
+
+        //    for (int i = 0; i <= s.Length - 1; i++)
+        //    {
+        //        char c = s.ElementAt(i);
+        //        if (c == '(')
+        //        {
+        //            int[] a = { i, 0 };
+        //            stack.Push(a);
+        //        }
+        //        else
+        //        {
+        //            if (stack.Count == 0 || stack.Peek()[1] == 1)
+        //            {
+        //                int[] a = { i, 1 };
+        //                stack.Push(a);
+        //            }
+        //            else
+        //            {
+        //                stack.Pop();
+        //                int currentLen = 0;
+        //                if (stack.Count == 0)
+        //                {
+        //                    currentLen = i + 1;
+        //                }
+        //                else
+        //                {
+        //                    currentLen = i - stack.Peek()[0];
+        //                }
+        //                result = Math.Max(result, currentLen);
+        //            }
+        //        }
+        //    }
+
+        //    return result;
+        //}
+    }
+}
---
+++ Algorithms/WordLadder/WordLadder/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WordLadder
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string start = "hit";
+            string end = "cog";
+            List<string> dict = new List<string>();
+
+            dict.Add("hot");
+            dict.Add("dot");
+            dict.Add("dog");
+            dict.Add("lot");
+            dict.Add("log");
+
+            Solution sol = new Solution();
+
+            Console.WriteLine(sol.ladderLength(start, end, dict));
+
+            Console.ReadKey();
+        }
+    }
+
+    public class WordNode
+    {
+        public string word;
+        public int numSteps;
+
+        public WordNode(string word, int numSteps)
+        {
+            this.word = word;
+            this.numSteps = numSteps;
+        }
+    }
+
+    public class Solution
+    {
+        public int ladderLength(string beginWord, string endWord, List<string> wordDict)
+        {
+            List<WordNode> queue = new List<WordNode>();
+            queue.Add(new WordNode(beginWord, 1));
+
+            wordDict.Add(endWord);
+
+            while (queue.Count != 0)
+            {
+                WordNode top = queue.First();
+                string word = top.word;
+
+                if (word.Equals(endWord))
+                {
+                    return top.numSteps;
+                }
+
+                char[] arr = word.ToCharArray();
+                for (int i = 0; i < arr.Count(); i++)
+                {
+                    for (char c = 'a'; c <= 'z'; c++)
+                    {
+                        char temp = arr[i];
+                        if (arr[i] != c)
+                        {
+                            arr[i] = c;
+                        }
+
+                        String newWord = new String(arr);
+                        if (wordDict.Contains(newWord))
+                        {
+                            queue.Add(new WordNode(newWord, top.numSteps + 1));
+                            wordDict.Remove(newWord);
+                        }
+
+                        arr[i] = temp;
+                    }
+                }
+            }
+
+            return 0;
+        }
+    }
+}

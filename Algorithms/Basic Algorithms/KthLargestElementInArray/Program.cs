89c9d68a369901b960c4106052ff959a1e0bb862
Aashish Bhateja <aashish.bhateja@gmail.com>
2017-02-20 18:56:45 -0800

    kth largest element

---
+++ Algorithms/Basic Algorithms/KthLargestElementInArray/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KthLargestElementInArray
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = {3, 2, 1, 5, 6, 4};
+
+            Console.WriteLine(KthLargestElement(3, arr));
+            Console.ReadKey();
+        }
+
+        public static int KthLargestElement(int k, int[] array)
+        {
+            List<int> list = new List<int>();
+            list = array.ToList();
+            list.Sort();
+
+            return list[list.Count-k];
+        }
+    }
+}
